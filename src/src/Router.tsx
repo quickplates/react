e0261d43a3812e622b72bd66cdb6431ff6d23913
Sebastian Pietras <me@spietras.dev>
2023-07-07 16:51:10 +0000

    Fixed base urls

--- src/src/Router.tsx
+++ src/src/Router.tsx
@@ -2,19 +2,24 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { ErrorPage, IndexPage, Root } from "./pages";
 
 export function Router() {
-  const router = createBrowserRouter([
+  const router = createBrowserRouter(
+    [
+      {
+        path: "/",
+        element: <Root />,
+        errorElement: <ErrorPage />,
+        children: [
+          {
+            index: true,
+            element: <IndexPage />,
+          },
+        ],
+      },
+    ],
     {
-      path: "/",
-      element: <Root />,
-      errorElement: <ErrorPage />,
-      children: [
-        {
-          index: true,
-          element: <IndexPage />,
-        },
-      ],
-    },
-  ]);
+      basename: import.meta.env.BASE_URL,
+    }
+  );
 
   return <RouterProvider router={router} />;
 }
